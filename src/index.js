6bd76d3886371d410710a09c4e4b3ca7e0e3535f
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-06-25 16:20:00 -0400

    rearange

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import App from './App';
 import './styles/style.css';
 import './styles/images.css';
 import './styles/no-touch.min.css';
-import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
     <App  />,
-    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
